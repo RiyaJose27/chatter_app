e9f92b39a2616fcaee443391441e558e0e10eaad
Riya Jose Pratheeksh <rjosepratheeksh@gmail.com>
2021-09-13 15:48:25 -0500

    private_wall

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -5,7 +5,7 @@ class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', # change the user and password as needed
-                                    password = 'Chr1st27', 
+                                    password = 'root1', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
